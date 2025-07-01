438ef03d67b2ef629df534a5d0f6f900e44a26fd
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-07-01 22:10:17 +0600

    made a RefForm compo and used a hook called useRef() with inidial value empty string and set it into each input filed via ref=() and also focused first form field via current.focus() function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import RefForm from './components/RefForm/RefForm'
 import SimpleForm from './components/SimpleForm/SimpleForm'
 import StateFulForm from './components/StateFulForm/StateFulForm'
 
@@ -9,10 +10,13 @@ function App() {
   return (
     <div className='flex flex-col items-center h-screen bg-gray-100'>
       <h1 className='text-4xl font-bold my-10'>Welcome to React Form Learning</h1>
-      <SimpleForm></SimpleForm>
+      {/* <SimpleForm></SimpleForm> */}
 
-      <h2 className='text-2xl font-bold mt-10'>Stateful Form</h2>
-      <StateFulForm></StateFulForm>
+      {/* <h2 className='text-2xl font-bold mt-10'>Stateful Form</h2> */}
+      {/* <StateFulForm></StateFulForm> */}
+
+      <h2 className='text-2xl font-bold mt-10'>Reff Form</h2>
+      <RefForm></RefForm>
      
     </div>
   )
---
+++ src/components/RefForm/RefForm.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from "react";
+
+
+const RefForm = () => {
+
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    useEffect(() => {
+        nameRef.current.focus()
+    }, []);
+
+    const handleOnSubmit = (event) => {
+        event.preventDefault();
+        console.log(nameRef.current.value);
+        console.log(emailRef.current.value);
+        console.log(passwordRef.current.value); 
+    }
+
+    return (
+        <div>
+              <form onSubmit={handleOnSubmit}  className='flex flex-col gap-y-2'>
+                <input ref={nameRef} className='border px-2' type="text" name="name" placeholder='james cameron' required/>
+
+                <input ref={emailRef} className='border px-2'  type="email" name="email"  placeholder='email address' required/>
+
+                <input ref={passwordRef} className='border px-2'  type="password" name="password"  placeholder='password' required/>  
+        
+
+                <input className='bg-green-500' type="submit" value="Submit" />
+
+           </form>
+        </div>
+    );
+};
+
+export default RefForm;

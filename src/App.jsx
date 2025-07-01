04ece60248736f57e37374c5b26cf85e2be71b94
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-07-01 21:48:30 +0600

    handle form using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import SimpleForm from './components/SimpleForm/SimpleForm'
+import StateFulForm from './components/StateFulForm/StateFulForm'
 
 function App() {
 
@@ -9,6 +10,9 @@ function App() {
     <div className='flex flex-col items-center h-screen bg-gray-100'>
       <h1 className='text-4xl font-bold my-10'>Welcome to React Form Learning</h1>
       <SimpleForm></SimpleForm>
+
+      <h2 className='text-2xl font-bold mt-10'>Stateful Form</h2>
+      <StateFulForm></StateFulForm>
      
     </div>
   )
---
+++ src/components/StateFulForm/StateFulForm.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const StateFulForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(''); 
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+        console.log(name);
+        console.log(email);
+        console.log(password);
+
+        if(password.length <6){
+            setError('Password must be at least 6 characters long');
+        }
+        else{
+            setError('');
+            // Here you can handle the form submission, like sending data to a server
+            console.log('Form submitted successfully');
+        }
+    
+    }
+   
+    const handleNameChange = e => {
+          setName(e.target.value);
+        
+    }
+    const handleEmailChange = e => {
+        setEmail(e.target.value);
+    }
+    const handlePasswordChange = e => {
+        setPassword(e.target.value);
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleOnSubmit}  className='flex flex-col gap-y-2'>
+                <input onChange={handleNameChange} className='border px-2' type="text" name="name" placeholder='james cameron' required/>
+
+                <input onChange={handleEmailChange} className='border px-2'  type="email" name="email"  placeholder='email address' required/>
+
+                <input onChange={handlePasswordChange} className='border px-2'  type="password" name="password"  placeholder='password' required/> 
+        
+
+                <input className='bg-green-500' type="submit" value="Submit" />
+
+                {error ? error : 'Form submitted successfully'} 
+           </form>
+        </div>
+    );
+};
+
+export default StateFulForm;
